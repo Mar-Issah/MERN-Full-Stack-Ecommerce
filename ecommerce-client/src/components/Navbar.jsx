81c0ad6b46053aaa0a380042089d0d624c50906a
Mar-Issah <masy370@gmail.com>
2022-03-19 14:25:27 -0400

    Install: styled components

--- ecommerce-client/src/components/Navbar.jsx
+++ ecommerce-client/src/components/Navbar.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import {
+  Container,
+  Wrapper,
+  Left,
+  Center,
+  Right,
+  Language,
+  SearchContainer,
+  Input,
+} from '../styled/home.styled';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder='Search' />
+            {/* <Search style={{ color: 'gray', fontSize: 16 }} /> */}
+          </SearchContainer>
+        </Left>
+        <Center></Center>
+        <Right></Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
