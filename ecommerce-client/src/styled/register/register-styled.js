5ab9179847d4becc57f95820b70e9328dd6c4767
Mar-Issah <masy370@gmail.com>
2022-03-23 19:40:09 -0400

    Mobile responsive register page

--- ecommerce-client/src/styled/register/register-styled.js
+++ ecommerce-client/src/styled/register/register-styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { mobile } from '../../responsive';
 
 export const Container = styled.div`
   width: 100vw;
@@ -14,6 +15,7 @@ export const Wrapper = styled.div`
   width: 40%;
   padding: 20px;
   background-color: #fff;
+  ${mobile({ width: '75%' })}
 `;
 
 export const Title = styled.h1`
