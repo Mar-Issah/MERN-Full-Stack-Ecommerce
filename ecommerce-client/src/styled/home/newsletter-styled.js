602204934a8297493362e3cf9bdf400789497c17
Mar-Issah <masy370@gmail.com>
2022-03-23 19:31:15 -0400

    Mobile responsive newsletter

--- ecommerce-client/src/styled/home/newsletter-styled.js
+++ ecommerce-client/src/styled/home/newsletter-styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { mobile } from '../../responsive';
 
 export const Container = styled.div`
   height: 40vh;
@@ -8,16 +9,19 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  ${mobile({ height: '20vh' })}
 `;
 export const Title = styled.h1`
   font-size: 70px;
   margin-bottom: 20px;
+  ${mobile({ fontSize: '50px' })}
 `;
 
 export const Desc = styled.div`
   font-size: 24px;
   font-weight: 300;
   margin-bottom: 20px;
+  ${mobile({ textAlign: 'center' })}
 `;
 
 export const InputContainer = styled.div`
@@ -29,6 +33,7 @@ export const InputContainer = styled.div`
   border: 1px solid lightgray;
   flex: 8;
   padding-left: 20px;
+  ${mobile({ width: '80%' })}
 `;
 export const Input = styled.input`
   border: none;
