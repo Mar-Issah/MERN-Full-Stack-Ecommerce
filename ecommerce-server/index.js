8989c2deacf3500bd246a8db0cb8027344cbbf7b
Mar-Issah <masy370@gmail.com>
2022-03-24 11:10:28 -0400

    Add user route

--- ecommerce-server/index.js
+++ ecommerce-server/index.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT;
 const mongoose = require('mongoose');
+const userRoute = require('./routes/user');
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Hello World!' });
-});
+app.use('/api/users', userRoute);
 
 const uri = process.env.MONGO_URI;
-
+const port = process.env.PORT || 5000;
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Mongodb database connected successfully'))
