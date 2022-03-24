ef59334c35ab5185ceb0a676ce70fbbc55ec7c4f
Mar-Issah <masy370@gmail.com>
2022-03-23 22:40:56 -0400

    Database: mongoose connection

--- ecommerce-server/index.js
+++ ecommerce-server/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+require('dotenv').config();
+const port = process.env.PORT;
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+const uri = process.env.MONGO_URI;
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Mongodb database connected successfully'))
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
