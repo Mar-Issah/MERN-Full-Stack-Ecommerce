fdc27e5af83179ce602a73a25dcb47c47dd09ee7
Mar-Issah <masy370@gmail.com>
2022-03-24 21:16:53 -0400

    Create jwt in login route

--- ecommerce-server/routes/login.js
+++ ecommerce-server/routes/login.js
@@ -4,25 +4,39 @@ const CryptoJS = require('crypto-js');
 const jwt = require('jsonwebtoken');
 
 //create all CRUD route relating login
-//user enters username and password from client and post it
-//since the user is registered. There is record of username and password in db
 router.post('/', async (req, res) => {
   try {
     const username = req.body.username;
-    const password = req.body.password;
+    const pass = req.body.password;
 
-    //find user in db. this will return user with the registered fields
     const user = await User.findOne({ username: username });
-
-    //401: unauthorized if no user found in db
-    !user && res.status(401).json('Wrong credentials!');
+    if (!user) {
+      return res.status(401).json('Wrong credentials!');
+    }
 
     //above returns encrypted password so lets decrypt it
     const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SECRET);
 
     const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-    // originalPassword !== password && res.status(401).json('Wrong credentials!');
-    res.status(200).json({ originalPassword });
+    if (originalPassword !== pass) {
+      return res.status(401).json('Wrong credentials!');
+    }
+
+    //after the user  is found, we want to send the user to the client, but we want to hide the password field that when jwt cames in
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '3d' }
+    );
+
+    //remove the password and send other info
+    //user._doc because mongo db stores othe unnecessary fields
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
   } catch (error) {
     res.status(500).json(error);
   }
