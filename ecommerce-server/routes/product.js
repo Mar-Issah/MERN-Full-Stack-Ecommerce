a3e3e720a5b5ab77115ae00a42eca5eecb915e84
Mar-Issah <masy370@gmail.com>
2022-04-03 09:00:59 -0400

    Create update and delete product route

--- ecommerce-server/routes/product.js
+++ ecommerce-server/routes/product.js
@@ -7,7 +7,6 @@ const router = require('express').Router();
 //only an admin can create a product
 router.post('/create', verifyTokenAndAdmin, async (req, res) => {
   const newProduct = new Product(req.body);
-
   try {
     const savedProduct = await newProduct.save();
     res.status(200).json(savedProduct);
@@ -16,4 +15,29 @@ router.post('/create', verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+//UPDATE
+router.put('/:id', verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json('Product has been deleted...');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
