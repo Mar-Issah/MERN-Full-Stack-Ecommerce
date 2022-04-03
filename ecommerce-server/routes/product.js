8430c6e7e8aa9a3c3ef3e1cccbdbd70ac3c46006
Mar-Issah <masy370@gmail.com>
2022-04-03 09:10:19 -0400

    Create findById product route

--- ecommerce-server/routes/product.js
+++ ecommerce-server/routes/product.js
@@ -1,6 +1,7 @@
 const Product = require('../models/Product');
 const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verifyTokenMiddleware');
-
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
 const router = require('express').Router();
 
 //CREATE
@@ -40,4 +41,42 @@ router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//GET PRODUCT
+//we dont need middleware anybody can view product
+router.get('/find/:id', async (req, res) => {
+  try {
+    const product = await Product.findById({ _id: ObjectId(req.params.id) });
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL PRODUCTS
+//we dont need middleware anybody can view product
+router.get('/', async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
+
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(1);
+    } else if (qCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
