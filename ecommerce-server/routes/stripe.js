3955eb0408c849b7cfdec4501d21c0ec39ff7e44
Mar-Issah <masy370@gmail.com>
2022-04-08 18:36:46 -0400

    Install stripe and create stripe route

--- ecommerce-server/routes/stripe.js
+++ ecommerce-server/routes/stripe.js
@@ -5,21 +5,23 @@ const stripe = require('stripe')(KEY);
 //stripe is most popular but you can use others like paypal etc
 //https://stripe.com/ create an account =>got to developer tab
 //Api keys => publishable key (FE) and secret key (BE)
+
 router.post('/payment', (req, res) => {
-  stripe.charges.create(
-    {
-      source: req.body.tokenId,
-      amount: req.body.amount,
-      currency: 'usd',
-    },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        res.status(500).json(stripeErr);
-      } else {
-        res.status(200).json(stripeRes);
-      }
-    }
-  );
+  res.status(200).json({ stripe: 'stripe route' });
+  // stripe.charges.create(
+  //   {
+  //     source: req.body.tokenId,
+  //     amount: req.body.amount,
+  //     currency: 'usd',
+  //   },
+  //   (stripeErr, stripeRes) => {
+  //     if (stripeErr) {
+  //       res.status(500).json(stripeErr);
+  //     } else {
+  //       res.status(200).json(stripeRes);
+  //     }
+  //   }
+  // );
 });
 
 module.exports = router;
