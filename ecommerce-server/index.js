b91901b57ff9ebd5032ab387e70f295bd97f6bfd
Mar-Issah <masy370@gmail.com>
2022-04-07 23:11:48 -0400

    Add create new cart route

--- ecommerce-server/index.js
+++ ecommerce-server/index.js
@@ -6,7 +6,7 @@ const userRoute = require('./routes/user');
 const registerRoute = require('./routes/register');
 const loginRoute = require('./routes/login');
 const productRoute = require('./routes/product');
-// const cartRoute = require('./routes/cart');
+const cartRoute = require('./routes/cart');
 // const orderRoute = require('./routes/order');
 // const stripeRoute = require('./routes/stripe');
 
@@ -16,7 +16,7 @@ app.use('/api/register', registerRoute);
 app.use('/api/login', loginRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
-// app.use('/api/carts', cartRoute);
+app.use('/api/carts', cartRoute);
 // app.use('/api/orders', orderRoute);
 // app.use('/api/checkout', stripeRoute);
 
