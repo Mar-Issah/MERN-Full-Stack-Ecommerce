5bcb04f9a05abd9a6dc6fb3e072894290e01d0e7
Mar-Issah <masy370@gmail.com>
2022-04-08 14:00:08 -0400

    Create: find order by user id route

--- ecommerce-server/routes/order.js
+++ ecommerce-server/routes/order.js
@@ -42,14 +42,15 @@ router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
 });
 
 //GET USER ORDERS
-// router.get('/find/:userId', verifyTokenAndAuthorization, async (req, res) => {
-//   try {
-//     const orders = await Order.find({ userId: req.params.userId });
-//     res.status(200).json(orders);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//find by userid not order id
+router.get('/find/:userId', verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.params.userId });
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // //GET ALL
 
