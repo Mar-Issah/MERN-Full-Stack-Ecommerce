223a5dd3af6b0997439fabbd04d6e4bd32570ebd
Mar-Issah <masy370@gmail.com>
2022-04-08 15:28:16 -0400

    Create: find find aggregat  order income for last two month route

--- ecommerce-server/routes/order.js
+++ ecommerce-server/routes/order.js
@@ -68,40 +68,47 @@ router.get('/', verifyTokenAndAdmin, async (req, res) => {
 });
 
 // GET MONTHLY INCOME
+//income if you  want to return income made from orders in a given period/ month
+//make sure the prodeuct id matches that from the query $elematch
+//use the aggregate which accepts an array of conditons. first get the last month and the month before
+//we are comparing incomes of those months
+//$match (like js find) and return createdAt greater than the last 2months (jus returning last 2months orders)
+//$project creates a new field/column in the document called month wwhich is the month retrieved from the createdAt date and another column sales which is the Order amount
+//$group (like JS reduce), returns id which is a no. represnetation of the month and also sum of all the sales create in the monthh
 
-// router.get('/income', verifyTokenAndAdmin, async (req, res) => {
-//   const productId = req.query.pid;
-//   const date = new Date();
-//   const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
-//   const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+router.get('/income', verifyTokenAndAdmin, async (req, res) => {
+  const productId = req.query.pid;
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
 
-//   try {
-//     const income = await Order.aggregate([
-//       {
-//         $match: {
-//           createdAt: { $gte: previousMonth },
-//           ...(productId && {
-//             products: { $elemMatch: { productId } },
-//           }),
-//         },
-//       },
-//       {
-//         $project: {
-//           month: { $month: '$createdAt' },
-//           sales: '$amount',
-//         },
-//       },
-//       {
-//         $group: {
-//           _id: '$month',
-//           total: { $sum: '$sales' },
-//         },
-//       },
-//     ]);
-//     res.status(200).json(income);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+  try {
+    const income = await Order.aggregate([
+      {
+        $match: {
+          createdAt: { $gte: previousMonth },
+          // ...(productId && {
+          //   products: { $elemMatch: { productId } },
+          //}),
+        },
+      },
+      {
+        $project: {
+          month: { $month: '$createdAt' },
+          sales: '$amount',
+        },
+      },
+      {
+        $group: {
+          _id: '$month', //month e.g 3(mar), 4(apr)
+          total: { $sum: '$sales' }, //total sales for each month
+        },
+      },
+    ]);
+    res.status(200).json(income);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
