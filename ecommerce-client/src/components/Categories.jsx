28dd8e927070d7290a3b8860f2db5cbe0ebc1688
Mar-Issah <masy370@gmail.com>
2022-04-11 19:18:14 -0400

    Complete: categories component

--- ecommerce-client/src/components/Categories.jsx
+++ ecommerce-client/src/components/Categories.jsx
@@ -1,18 +1,21 @@
 import { categories } from '../data';
 import { Wrapper, Container, Image, Title, Info, Button } from '../styled/home/categories-styled';
+import { Link } from 'react-router-dom';
+
+//user clicks on each catgory card it take user to the `/products/${item.cat} route created. path params
 
 const Categories = () => {
   return (
     <Container>
       {categories.map((item) => (
         <Wrapper key={item.id}>
-          <a href={`/products/${item.cat}`}>
+          <Link href={`/products/${item.cat}`}>
             <Image src={item.img} />
             <Info>
               <Title>{item.title}</Title>
               <Button>SHOP NOW</Button>
             </Info>
-          </a>
+          </Link>
         </Wrapper>
       ))}
     </Container>
