e82724aa70196b587bf71264b86369df938d147e
Mar-Issah <masy370@gmail.com>
2022-04-16 21:11:42 -0400

    Add UserList page to react admin

--- react-admin/src/components/widgetSm/WidgetSm.jsx
+++ react-admin/src/components/widgetSm/WidgetSm.jsx
@@ -1,83 +1,63 @@
-import "./widgetSm.css";
-import { Visibility } from "@material-ui/icons";
+import './widgetSm.css';
+import { Visibility } from '@material-ui/icons';
 
 export default function WidgetSm() {
   return (
-    <div className="widgetSm">
-      <span className="widgetSmTitle">New Join Members</span>
-      <ul className="widgetSmList">
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
+    <div className='widgetSm'>
+      <span className='widgetSmTitle'>New Join Members</span>
+      <ul className='widgetSmList'>
+        <li className='widgetSmListItem'>
+          <img src='https://avatars.githubusercontent.com/Mar-Issah' alt='' className='widgetSmImg' />
+          <div className='widgetSmUser'>
+            <span className='widgetSmUsername'>Anna Keller</span>
+            <span className='widgetSmUserTitle'>Software Engineer</span>
           </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
+          <button className='widgetSmButton'>
+            <Visibility className='widgetSmIcon' />
             Display
           </button>
         </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
+        <li className='widgetSmListItem'>
+          <img src='https://avatars.githubusercontent.com/Mar-Issah' alt='' className='widgetSmImg' />
+          <div className='widgetSmUser'>
+            <span className='widgetSmUsername'>Anna Keller</span>
+            <span className='widgetSmUserTitle'>Software Engineer</span>
           </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
+          <button className='widgetSmButton'>
+            <Visibility className='widgetSmIcon' />
             Display
           </button>
         </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
+        <li className='widgetSmListItem'>
+          <img src='https://avatars.githubusercontent.com/Mar-Issah' alt='' className='widgetSmImg' />
+          <div className='widgetSmUser'>
+            <span className='widgetSmUsername'>Anna Keller</span>
+            <span className='widgetSmUserTitle'>Software Engineer</span>
           </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
+          <button className='widgetSmButton'>
+            <Visibility className='widgetSmIcon' />
             Display
           </button>
         </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
+        <li className='widgetSmListItem'>
+          <img src='https://avatars.githubusercontent.com/Mar-Issah' alt='' className='widgetSmImg' />
+          <div className='widgetSmUser'>
+            <span className='widgetSmUsername'>Anna Keller</span>
+            <span className='widgetSmUserTitle'>Software Engineer</span>
           </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
+          <button className='widgetSmButton'>
+            <Visibility className='widgetSmIcon' />
             Display
           </button>
         </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
+        <li className='widgetSmListItem'>
+          <img src='https://avatars.githubusercontent.com/Mar-Issah' alt='' className='widgetSmImg' />
+          <div className='widgetSmUser'>
+            <span className='widgetSmUsername'>Anna Keller</span>
+            <span className='widgetSmUserTitle'>Software Engineer</span>
           </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
+          <button className='widgetSmButton'>
+            <Visibility className='widgetSmIcon' />
             Display
           </button>
         </li>
