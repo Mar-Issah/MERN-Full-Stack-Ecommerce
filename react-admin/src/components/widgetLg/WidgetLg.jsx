cc8cf020b9c9b5257d345ddacba2ef1c76ad8507
Mar-Issah <masy370@gmail.com>
2022-04-21 15:06:32 -0400

    Install timeago.js lib to format createdAt date

--- react-admin/src/components/widgetLg/WidgetLg.jsx
+++ react-admin/src/components/widgetLg/WidgetLg.jsx
@@ -1,6 +1,22 @@
 import './widgetLg.css';
+import { useState, useEffect } from 'react';
+import { userRequest } from '../../axiosInstance';
 
 export default function WidgetLg() {
+  const [orders, setOrders] = useState([]);
+
+  //on component mount or update get orders to populatr the table in jsx
+  //In orther to get the user image we can get userId from order data and use id to find user
+  useEffect(() => {
+    const getOrders = async () => {
+      try {
+        const res = await userRequest.get('/orders');
+        setOrders(res.data);
+      } catch (err) {}
+    };
+    getOrders();
+  }, []);
+  console.log(orders);
   const Button = ({ type }) => {
     return <button className={'widgetLgButton ' + type}>{type}</button>;
   };
@@ -17,50 +33,19 @@ export default function WidgetLg() {
           </tr>
         </thead>
         <tbody>
-          <tr className='widgetLgTr'>
-            <td className='widgetLgUser'>
-              <img src='https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt='' className='widgetLgImg' />
-              <span className='widgetLgName'>Susan Carol</span>
-            </td>
-            <td className='widgetLgDate'>2 Jun 2021</td>
-            <td className='widgetLgAmount'>$122.00</td>
-            <td className='widgetLgStatus'>
-              <Button type='Approved' />
-            </td>
-          </tr>
-          <tr className='widgetLgTr'>
-            <td className='widgetLgUser'>
-              <img src='https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt='' className='widgetLgImg' />
-              <span className='widgetLgName'>Susan Carol</span>
-            </td>
-            <td className='widgetLgDate'>2 Jun 2021</td>
-            <td className='widgetLgAmount'>$122.00</td>
-            <td className='widgetLgStatus'>
-              <Button type='Declined' />
-            </td>
-          </tr>
-          <tr className='widgetLgTr'>
-            <td className='widgetLgUser'>
-              <img src='https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt='' className='widgetLgImg' />
-              <span className='widgetLgName'>Susan Carol</span>
-            </td>
-            <td className='widgetLgDate'>2 Jun 2021</td>
-            <td className='widgetLgAmount'>$122.00</td>
-            <td className='widgetLgStatus'>
-              <Button type='Pending' />
-            </td>
-          </tr>
-          <tr className='widgetLgTr'>
-            <td className='widgetLgUser'>
-              <img src='https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt='' className='widgetLgImg' />
-              <span className='widgetLgName'>Susan Carol</span>
-            </td>
-            <td className='widgetLgDate'>2 Jun 2021</td>
-            <td className='widgetLgAmount'>$122.00</td>
-            <td className='widgetLgStatus'>
-              <Button type='Approved' />
-            </td>
-          </tr>
+          {orders.map((order) => (
+            <tr className='widgetLgTr' key={order._id}>
+              <td className='widgetLgUser'>
+                {/* <img src='https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt='' className='widgetLgImg' /> */}
+                <span className='widgetLgName'>{order.userId}</span>
+              </td>
+              <td className='widgetLgDate'>{order.createdAt}</td>
+              <td className='widgetLgAmount'>${order.amount}.00</td>
+              <td className='widgetLgStatus'>
+                <Button type={order.status} />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
