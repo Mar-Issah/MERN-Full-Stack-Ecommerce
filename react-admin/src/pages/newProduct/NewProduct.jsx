c5cadfa4fae287a76b7f3d51f6f463b68dde3748
Mar-Issah <masy370@gmail.com>
2022-04-24 21:38:08 -0400

    Install firebase

--- react-admin/src/pages/newProduct/NewProduct.jsx
+++ react-admin/src/pages/newProduct/NewProduct.jsx
@@ -1,30 +1,61 @@
-import "./newProduct.css";
+import './newProduct.css';
+import { useState } from 'react';
 
+//shows how to handle multple inputs in one state
+//categories are seperated because it is an array
 export default function NewProduct() {
+  const [inputs, setInputs] = useState({});
+  const [file, setFile] = useState(null);
+  const [cat, setCat] = useState([]);
+
+  //handling muliple inputs using name
+  //prev; make sure it doesnt reuturn a new object since there are multiple fields
+  //make sure name are exact name in db
+  const handleChange = (e) => {
+    console.log(e);
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+  //use the split method to  seprate the CSV
+  const handleCategories = (e) => {
+    setCat(e.target.value.split(','));
+  };
+  console.log(inputs);
+  console.log(cat);
+
   return (
-    <div className="newProduct">
-      <h1 className="addProductTitle">New Product</h1>
-      <form className="addProductForm">
-        <div className="addProductItem">
+    <div className='newProduct'>
+      <h1 className='addProductTitle'>New Product</h1>
+      <form className='addProductForm'>
+        <div className='addProductItem'>
           <label>Image</label>
-          <input type="file" id="file" />
+          <input type='file' id='file' onChange={(e) => setFile(e.target.files)[0]} />
         </div>
-        <div className="addProductItem">
-          <label>Name</label>
-          <input type="text" placeholder="Apple Airpods" />
+        <div className='addProductItem'>
+          <label>Title</label>
+          <input name='title' type='text' placeholder='Apple Airpods' onChange={handleChange} />
         </div>
-        <div className="addProductItem">
+        <div className='addProductItem'>
+          <label>Decription</label>
+          <input name='desc' type='text' placeholder='Description' onChange={handleChange} />
+        </div>
+        <div className='addProductItem'>
+          <label>Price</label>
+          <input name='price' type='text' placeholder='Price' onChange={handleChange} />
+        </div>
+        <div className='addProductItem'>
+          <label>Categories</label>
+          <input type='text' placeholder='Coat, Jeans' onChange={handleCategories} />
+        </div>
+        <div className='addProductItem'>
           <label>Stock</label>
-          <input type="text" placeholder="123" />
-        </div>
-        <div className="addProductItem">
-          <label>Active</label>
-          <select name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
+          <select name='inStock' onChange={handleChange}>
+            <option value='true'>Yes</option>
+            <option value='false'>No</option>
           </select>
         </div>
-        <button className="addProductButton">Create</button>
+        <button className='addProductButton'>Create</button>
       </form>
     </div>
   );
